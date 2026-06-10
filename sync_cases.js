f7c0f689cd78c9dbbc6c5f19a7c8c4c5290288ad
Sergio Vázquez <tu-correo@interlub.com>
2026-06-10 11:03:33 -0600

    Update HAI success case to Nov 2023 trial with concise plant description and layout matching Bonnell

--- sync_cases.js
+++ sync_cases.js
@@ -1,4 +1,4 @@
-﻿const fs = require('fs');
+const fs = require('fs');
 
 const dataJs = fs.readFileSync('data.js', 'utf8');
 const indexHtml = fs.readFileSync('index.html', 'utf8');
@@ -8,22 +8,24 @@ const matchData = dataJs.match(/const casesData = \{[\s\S]*?\};\r?\n\/\* end cas
 if (matchData) {
   let newHtml = indexHtml.replace(/const casesData = \{[\s\S]*?\};\r?\n\s*\/\* end casesData \*\//, matchData[0]);
   
+  // Extract casesRegistry block from data.js
+  const matchRegistry = dataJs.match(/const casesRegistry = \[[\s\S]*?\];/);
+  if (matchRegistry) {
+    newHtml = newHtml.replace(/const casesRegistry = \[[\s\S]*?\];/, matchRegistry[0]);
+  }
+
+  // Extract homeContent block from data.js
+  const matchHomeContent = dataJs.match(/const homeContent = \{[\s\S]*?\};/);
+  if (matchHomeContent) {
+    newHtml = newHtml.replace(/const homeContent = \{[\s\S]*?\};/, matchHomeContent[0]);
+  }
+
   // Fix overflow: hidden
   newHtml = newHtml.replace('      display: flex;\r\n      flex-direction: column;\r\n      position: relative;\r\n      overflow: hidden;\r\n    }', '      display: flex;\r\n      flex-direction: column;\r\n      position: relative;\r\n    }');
   newHtml = newHtml.replace('      display: flex;\n      flex-direction: column;\n      position: relative;\n      overflow: hidden;\n    }', '      display: flex;\n      flex-direction: column;\n      position: relative;\n    }');
   
-  // Update registry
-  newHtml = newHtml.replace('location:  "San Nicol\u00E1s, NL, M\u00E9xico",', 'location:  "San Nicolas, Nuevo Leon, Mexico",');
-  newHtml = newHtml.replace('location:  "Honduras",', 'location:  "San Pedro Sula, Honduras, Honduras",');
-  newHtml = newHtml.replace('location:  "Cressona, PA, USA",', 'location:  "Cressona, Pennsylvania, United States",');
-  
-  // Replace the Confidential EU with HAI
-  const confidentialEuRegex = /\{\s*id:\s*"confidential-eu"[\s\S]*?isCurrent:\s*false\s*\}/;
-  const haiRegistry = '{\n      id:        "hai",\n      client:    "HAI",\n      industry:  "Aluminum Extrusion",\n      location:  "Arad County, Romania, Romania",\n      headline:  "100% elimination of crystallized deposits on flagship press",\n      date:      "2023-11",\n      isCurrent: true\n    }';
-  newHtml = newHtml.replace(confidentialEuRegex, haiRegistry);
-  
   fs.writeFileSync('index.html', newHtml, 'utf8');
-  console.log('index.html synced successfully');
+  console.log('index.html synced successfully (including casesData, casesRegistry, and homeContent)');
 } else {
   console.log('Could not find casesData in data.js');
 }
